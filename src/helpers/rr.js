c6a89dc759180686309bd6b084e60be4986c3498
Matheus Biazotto <83847093+MatheusBiazotto@users.noreply.github.com>
2023-04-24 20:08:46 -0300

    Update rr.js

--- src/helpers/rr.js
+++ src/helpers/rr.js
@@ -1,10 +1,23 @@
-export function rr(arrivalTime, burstTime, timeQuantum) {
-  const processesInfo = arrivalTime
+/**
+ * Função para executar o algoritmo de escalonamento Round Robin preemptivo
+ * @param {Array<Number>} tempoChegada
+ * @param {Array<Number>} tempoExecucao
+ * @param {Number} quantum
+ * @returns {List<{id: Number, nome: String, at: Number, tempoExecucao: Number, tempoEspera: Number, tempoTurnaround: Number }>}
+ */
+export function executeRoundRobin(tempoExecucao, quantum) {
+  /**
+   * Mapeamento e ordenação de processos pelo tempo de chegada
+   * Neste caso, o tempo de chegada é pré-configurado 0 para todos os processos
+   */
+  const infoProcessos = tempoExecucao
+    .map(() => 0)
     .map((item, index) => {
       return {
-        job: (index + 10).toString(36).toUpperCase(),
+        id: index + 1,
+        nome: (index + 10).toString(36).toUpperCase(),
         at: item,
-        bt: Number(burstTime[index])
+        tempoExecucao: Number(tempoExecucao[index])
       }
     })
     .sort((obj1, obj2) => {
@@ -13,101 +26,85 @@ export function rr(arrivalTime, burstTime, timeQuantum) {
       return 0
     })
 
-  const solvedProcessesInfo = []
-  const ganttChartInfo = []
+  const processos = []
 
-  const readyQueue = []
-  let currentTime = processesInfo[0].at
-  const unfinishedJobs = [...processesInfo]
+  const filaPronto = []
+  let tempoAtual = infoProcessos[0].at
+  const processosInacabados = [...infoProcessos]
 
-  const remainingTime = processesInfo.reduce((acc, process) => {
-    acc[process.job] = process.bt
+  const tempoRestante = infoProcessos.reduce((acc, process) => {
+    acc[process.nome] = process.tempoExecucao
     return acc
   }, {})
 
-  readyQueue.push(unfinishedJobs[0])
+  filaPronto.push(processosInacabados[0])
   while (
-    Object.values(remainingTime).reduce((acc, cur) => {
+    Object.values(tempoRestante).reduce((acc, cur) => {
       return acc + cur
     }, 0) &&
-    unfinishedJobs.length > 0
+    processosInacabados.length > 0
   ) {
-    if (readyQueue.length === 0 && unfinishedJobs.length > 0) {
-      // Previously idle
-      readyQueue.push(unfinishedJobs[0])
-      currentTime = readyQueue[0].at
+    if (filaPronto.length === 0 && processosInacabados.length > 0) {
+      // Anteriormente parado (idle)
+      filaPronto.push(processosInacabados[0])
+      tempoAtual = filaPronto[0].at
     }
 
-    const processToExecute = readyQueue[0]
+    const processoAtual = filaPronto[0]
 
-    if (remainingTime[processToExecute.job] <= timeQuantum) {
-      // Burst time less than or equal to time quantum, execute until finished
-      const remainingT = remainingTime[processToExecute.job]
-      remainingTime[processToExecute.job] -= Number(remainingT)
-      const prevCurrentTime = Number(currentTime)
-      currentTime += Number(remainingT)
-
-      ganttChartInfo.push({
-        job: processToExecute.job,
-        start: prevCurrentTime,
-        stop: currentTime
-      })
+    if (tempoRestante[processoAtual.nome] <= quantum) {
+      // Tempo restante menor ou igual ao quantum, execute até acabar
+      const remainingT = tempoRestante[processoAtual.nome]
+      tempoRestante[processoAtual.nome] -= Number(remainingT)
+      tempoAtual += Number(remainingT)
     } else {
-      remainingTime[processToExecute.job] -= Number(timeQuantum)
-      const prevCurrentTime = Number(currentTime)
-      currentTime += Number(timeQuantum)
-
-      ganttChartInfo.push({
-        job: processToExecute.job,
-        start: prevCurrentTime,
-        stop: currentTime
-      })
+      tempoRestante[processoAtual.nome] -= Number(quantum)
+      tempoAtual += Number(quantum)
     }
-    const processToArriveInThisCycle = processesInfo.filter((p) => {
+    const processToArriveInThisCycle = infoProcessos.filter((p) => {
       return (
-        p.at <= currentTime &&
-        p !== processToExecute &&
-        !readyQueue.includes(p) &&
-        unfinishedJobs.includes(p)
+        p.at <= tempoAtual &&
+        p !== processoAtual &&
+        !filaPronto.includes(p) &&
+        processosInacabados.includes(p)
       )
     })
 
-    // Push new processes to readyQueue
-    readyQueue.push(...processToArriveInThisCycle)
+    // Adicionando processo para fila de pronto
+    filaPronto.push(...processToArriveInThisCycle)
 
-    // Requeueing (move head/first item to tail/last)
-    readyQueue.push(readyQueue.shift())
+    // Realocando na fila
+    filaPronto.push(filaPronto.shift())
 
-    // When the process finished executing
-    if (remainingTime[processToExecute.job] === 0) {
-      const indexToRemoveUJ = unfinishedJobs.indexOf(processToExecute)
+    // Quando o processo é finalizado
+    if (tempoRestante[processoAtual.nome] === 0) {
+      const indexToRemoveUJ = processosInacabados.indexOf(processoAtual)
       if (indexToRemoveUJ > -1) {
-        unfinishedJobs.splice(indexToRemoveUJ, 1)
+        processosInacabados.splice(indexToRemoveUJ, 1)
       }
-      const indexToRemoveRQ = readyQueue.indexOf(processToExecute)
+      const indexToRemoveRQ = filaPronto.indexOf(processoAtual)
       if (indexToRemoveRQ > -1) {
-        readyQueue.splice(indexToRemoveRQ, 1)
+        filaPronto.splice(indexToRemoveRQ, 1)
       }
 
-      solvedProcessesInfo.push({
-        ...processToExecute,
-        ft: Number(currentTime),
-        tat: Number(currentTime) - Number(processToExecute.at),
-        wat: Number(currentTime) - Number(processToExecute.at) - Number(processToExecute.bt)
+      processos.push({
+        ...processoAtual,
+        ft: Number(tempoAtual),
+        tempoTurnaround: Number(tempoAtual) - Number(processoAtual.at),
+        tempoEspera:
+          Number(tempoAtual) - Number(processoAtual.at) - Number(processoAtual.tempoExecucao)
       })
     }
   }
 
-  // Sort the processes arrival time and then by job name
-  solvedProcessesInfo.sort((obj1, obj2) => {
+  // Organizando processo pelo nome
+  processos.sort((obj1, obj2) => {
     if (obj1.at > obj2.at) return 1
     if (obj1.at < obj2.at) return -1
-    if (obj1.job > obj2.job) return 1
-    if (obj1.job < obj2.job) return -1
+    if (obj1.nome > obj2.nome) return 1
+    if (obj1.nome < obj2.nome) return -1
     return 0
   })
 
-  console.log({ solvedProcessesInfo })
-
-  return { solvedProcessesInfo, ganttChartInfo }
+  return { processos }
 }
